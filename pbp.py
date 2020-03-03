773435eb9003da001f4c8bd334dd5507442444cd
James Armstrong <jjarmstrong@uchicago.edu>
2020-03-03 17:16:43 -0600

    Added momentum function

--- pbp.py
+++ pbp.py
@@ -18,7 +18,10 @@ def clean_text(pbp_url):
             for row in pbp_table.find_all("tr"):
                 for cell in row.find_all("td"):
                     if cell['class'] == ['time-stamp']:
-                        event_times.append(cell.text)
+                        if "." in cell.text:
+                            event_times.append("0:" + cell.text)
+                        else:
+                            event_times.append(cell.text + ".00")
                         event_quarters.append(current_quarter)
                     if cell['class'] == ['game-details']:
                         event_description.append(cell.text)
@@ -38,14 +41,42 @@ def clean_text(pbp_url):
 
     pbp_dataframe[["away_score", "home_score"]] = pbp_dataframe["score"].str.split("-", expand = True).astype('int64')
     pbp_dataframe = pbp_dataframe.drop(["score"], axis = 1)
+    #pbp_dataframe[["minutes, ""seconds"]] = pbp_dataframe["time"].str.split([":", "."], expand = True).astype('int64')
 
     return pbp_dataframe
 
 
+def calculate_momentum(pbp_dataframe):
+    pbp_with_momentum = pbp_dataframe.copy()
+    pbp_with_momentum["momentum"] = 0.0
+    pbp_dataframe["datetime"] = pd.to_datetime(pbp_dataframe["time"], format = "%M:%S.%f")
+    for play in pbp_dataframe.iterrows():
+        five_minutes_ago = play[1]["datetime"].minute + 5
+        if five_minutes_ago > 11:
+            if play[1]["quarter"] == 1:
+                pbp_with_momentum["momentum"][play[0]] = determine_momentum(play[1]["away_score"], play[1]["home_score"])
+            else:
+                play_five_minutes_ago = pbp_dataframe[(pbp_dataframe["quarter"] == (play[1]["quarter"] - 1)) &
+                                                      (pbp_dataframe["datetime"].dt.minute <= five_minutes_ago - 12) &
+                                                      (pbp_dataframe["datetime"].dt.second <= play[1]["datetime"].second)].head(1)
+                pbp_with_momentum["momentum"][play[0]] = determine_momentum(play[1]["away_score"] - play_five_minutes_ago["away_score"].iloc[0],
+                                                                            play[1]["home_score"] - play_five_minutes_ago["home_score"].iloc[0])        
+        else:
+            play_five_minutes_ago = pbp_dataframe[(pbp_dataframe["quarter"] == (play[1]["quarter"])) &
+                                                  (pbp_dataframe["datetime"].dt.minute <= five_minutes_ago) &
+                                                  (pbp_dataframe["datetime"].dt.second <= play[1]["datetime"].second)].head(1)
+            pbp_with_momentum["momentum"][play[0]] = determine_momentum(play[1]["away_score"] - play_five_minutes_ago["away_score"].iloc[0],
+                                                                        play[1]["home_score"] - play_five_minutes_ago["home_score"].iloc[0])
+            
+    return pbp_with_momentum
 
 
-
-
+def determine_momentum(away_points, home_points):
+    if away_points > home_points:
+        return -1 * float((away_points + 1) / (home_points + 1))
+    else:
+        return float((home_points + 1) / (away_points + 1))
+    
 def get_players(play_str):
     '''
     Input:  The text of a play (str)
