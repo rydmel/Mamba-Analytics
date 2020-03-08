feb9412ac6d57bf879eeeece1a34c9989850eb33
Jason Li <jasonli1@cs.uchicago.edu>
2020-03-07 19:04:37 -0600

    Added schedule and games attributes

--- Team.py
+++ Team.py
@@ -1,4 +1,14 @@
-class Game:
+import schedule as sch
+import Game
 
-    def __init__(self, name):
+
+class Team:
+
+    def __init__(self, name, season, season_type):
+    	self.name = name
+    	self.schedule = sch.get_team_schedule(name, season, season_type)
+    	self.games = [Game(game_id) for game_id in self.schedule['game_id']]
+
+    def 
+
     	
