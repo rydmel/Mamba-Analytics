fdb01830c8e0a8f18ee2ba8cfccc04d4882e3728
Ryan D'Mello <rdmello@cs.uchicago.edu>
2020-03-10 04:10:21 -0500

    finished docstring for Game.py

--- testproject/Game.py
+++ testproject/Game.py
@@ -7,6 +7,12 @@ import re
 class Game:
 
     def __init__(self, game_id):
+        '''
+        Constructor
+
+        Inputs:
+          game_id (string): Unique ID for ESPN game (string in URL)
+        '''
 
         self.url = "https://www.espn.com/nba/playbyplay?gameId=" + str(game_id)
         self.game_id = game_id
@@ -18,13 +24,25 @@ class Game:
         self.get_player_set()
         self.get_revised_play_list()
 
+
     def get_game_info(self):
+        '''    
+        Makes attributes df (Pandas DataFrame), away_team (str), home_team (str), home_logo (PNG image)    
+        '''
         self.df, self.away_team, self.away_logo, self.home_team, self.home_logo = clean_text(self.url)
 
+
     def get_momentum_df(self):
+        '''
+        Makes attribute momentum_df (Pandas DataFrame)    
+        '''
         self.momentum_df = calculate_momentum(self.df)
 
+
     def get_odds_info(self):
+        '''
+        Makes attributes line (str), over_under (str)    
+        '''
         odds_request = util.read_request(util.get_request("https://www.espn.com/nba/game?gameId={}".format(self.game_id)))
         odds_soup = bs4.BeautifulSoup(odds_request)
 
@@ -34,7 +52,13 @@ class Game:
             self.line = odds_section.find("li").text.strip("Line: ").lower()
             self.over_under = float(odds_section.find("li", class_ = "ou").text.strip("\n\t")[-3:])
             
+
     def get_player_set(self, mode = 'df'):
+        '''
+        Default input: mode = 'df'; any other value for mode uses momentum_df
+    
+        Makes attributes players_list (list), player_set (set), text (list)    
+        '''
         if mode == 'df':
             text = list(self.df['description'])    		
         else:
@@ -44,18 +68,31 @@ class Game:
         self.player_set = player_set
         self.text = text
 
+
     def get_revised_play_list(self):
+        '''
+        Makes attribute revised_play_list (list), which is a list of all the plays after players and numbers are cleaned
+        '''
         self.revised_play_list = make_new_text_list(self.text, self.players_list)
         
 
-    def parse_revised_text(self, s): 
+    def parse_revised_text(self, s):
+        '''    
+        Helper function called in get_player_dict
+        Inputs: s (str), which will be a revised play
+        Output: the type of shot attempt (str)
+        '''
         for x in ['makes', 'misses', '( assists)']: 
             s = s.replace(x, '') 
         s = s.lstrip() 
         s = s.strip() 
         return s
 
+
     def get_player_dict(self):
+        '''
+        Makes attribute shot_selection_dict (dict), which is a dict of players' types of shot attempts w/ frequency
+        '''
         self.shot_selection_dict = {}
         for i in range(len(self.revised_play_list)):
             play = self.revised_play_list[i]
